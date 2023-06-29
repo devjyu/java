0b3840d175f4e0a5b69f3795bfa9e47bea4bff4a
devjyu <devjyu@gmail.com>
2023-06-29 14:50:14 +0900

    부호연산자와 논리부정연산자

---
+++ ch03/S02.java
@@ -0,0 +1,19 @@
+package ch03;
+
+public class S02 {
+    public static void main(String[] args) {
+        int num = 10;
+
+        System.out.println(-num);//-10
+
+        int num1 = -20;
+
+        System.out.println(-num1);//20
+
+        boolean check = false;
+
+        System.out.println(!check);//true
+
+        
+    }
+}
