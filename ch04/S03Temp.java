54777664431810482201f37f76453aaa6fed846f
devjyu <devjyu@gmail.com>
2023-06-29 16:55:35 +0900

    switch문으로 음수양수 구별하기(설명추가)

--- ch04/S03Temp.java
+++ ch04/S03Temp.java
@@ -2,10 +2,15 @@ package ch04;
 
 public class S03Temp {
     public static void main(String[] args) {
-        int num = -17;
+        int num = 90;
 
+        // 2,3으로 해야 범위가 작아져서 switch문의 코드도 작아진다.
         System.out.println(num % 2);
+        // 나머지 -1, 0, 1 나오고
         System.out.println(num % 3);
+        // 나머지 -2, -1, 0, 1, 2 나오니까
+        // 둘이 합친다면 -1, -2, -3, 1, 2, 3이 나온다.
+        // case -1, -2, -3 이면 잘못, 그외의 것은 잘
 
         switch (num % 2 + num % 3){
             case -1:
