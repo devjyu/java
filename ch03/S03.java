5209f3f0c391557ff9dea61454960b46ab25b451
devjyu <devjyu@gmail.com>
2023-06-29 14:59:54 +0900

    이항연산자

---
+++ ch03/S03.java
@@ -0,0 +1,14 @@
+package ch03;
+
+public class S03 {
+    public static void main(String[] args) {
+        short a = 1;
+        short b = 2;
+        short c = (short)(a + b);
+
+        int num1 = 1000000;
+        int num2 = -2000000;
+        System.out.println(num1 * num2); //1454759936 부호가 바뀜
+
+    }
+}
