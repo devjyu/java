b66fe1a2035593f526eaccaeeaa81d949b3d1ab6
devjyu <devjyu@gmail.com>
2023-06-30 10:56:37 +0900

    for문 설명

---
+++ ch04/S09.java
@@ -0,0 +1,27 @@
+package ch04;
+
+public class S09 {
+    public static void main(String[] args) {
+        
+        // int i = 1;
+
+        // while (i <= 100000) {
+        // System.out.println(i);
+        // // 보통 기준이 되는 변수를 내부에서 조작 해야한다.
+        // i++;
+        // }
+
+        // 소괄호 첫번째 == 반복 변수 설정
+        // 소괄호 두번째 == 반복 종료 조건
+        // 소괄호 세번째 == 반복 변수 값 조정
+        // 소괄호 조건들은 세미콜론으로 나뉜다
+        for(int i = 1; i < 100; i++){
+            // for문 내부는 while과 똑같이
+            // break, continue 사용 가능
+        }
+        // 무한반복
+        // for(;;) {
+
+        // }
+    }
+}
