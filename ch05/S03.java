92153165bbbed103daa27afd951c32898a107bf9
devjyu <devjyu@gmail.com>
2023-06-30 16:03:12 +0900

    배열 값 넣는 방법

---
+++ ch05/S03.java
@@ -0,0 +1,17 @@
+package ch05;
+
+public class S03 {
+    public static void main(String[] args) {
+        // 배열 생성시 값을 넣는 방법
+        // int[] intArray = {10, 20, 30, 40};
+        // int[] intArray1 = new int[] {10, 20, 30, 40};
+    
+        // int[] intArray;
+        // intArray = {10, 20, 30, 40}; // 값을 참조함 // 에러 발생함
+
+        // int[] intArray1;
+        // intArray1 = new int[] {10, 20, 30, 40}; // new는 주소를 참조함
+
+        System.out.printf("%d / %d", 10,20);
+    }
+}
