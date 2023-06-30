a447d933c369a13683a0bae4237f3f8c9bb639d1
devjyu <devjyu@gmail.com>
2023-06-30 09:37:24 +0900

    while문 설명

---
+++ ch04/S05.java
@@ -0,0 +1,15 @@
+package ch04;
+
+public class S05 {
+    public static void main(String[] args) {
+        // 소괄호 안의 조건이 false가 될때까지
+        // 무한반복한다
+        // 소괄호 안에는 true나 false만 만 들어갈 수 있다.
+        while(true){
+            // 중괄호 안에는 반복할 코드를 작성한다.
+            // 어떤 조건에 따라 내부에서 반복을 멈추려면 break
+            // 어떤 조건에 따라 현재 위치에서 다음 반복으로 
+            // 넘어갈 경우에는 continue를 사용한다.
+        }
+    }
+}
