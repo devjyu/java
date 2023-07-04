c10020eb66924b964119c57da81f1e27f8719954
zxc8289 <zxc8289@naver.com>
2023-07-04 16:33:59 +0900

    하위 파일 불러오기

---
+++ ch07/S06.java
@@ -0,0 +1,11 @@
+package ch07;
+
+import ch07.sub.Sub01;
+
+public class S06 {
+    public static void main(String[] args) {
+        Sub01 sub01 = new Sub01();
+
+        System.out.println(sub01);
+    }
+}
