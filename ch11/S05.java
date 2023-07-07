179b6239574a0a832f7a655eebb3afa16f1da48e
zxc8289 <zxc8289@naver.com>
2023-07-07 17:57:03 +0900

    stack

---
+++ ch11/S05.java
@@ -0,0 +1,20 @@
+package ch11;
+
+import java.util.Stack;
+
+public class S05 {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>();
+
+        stack.push("사과");
+        stack.push("바나나");
+        stack.push("멜론");
+
+        System.out.println(stack); //[사과, 바나나, 멜론]
+
+        stack.pop();
+
+        System.out.println(stack); //[사과, 바나나]
+
+    }
+}
